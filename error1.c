f74518b21e385b6c093a0cf4858ead67c2120aeb
Nonku Khumalo <54401544+Nkuli002@users.noreply.github.com>
2022-11-16 23:49:07 +0200

    Update error1.c

--- error1.c
+++ error1.c
@@ -43,7 +43,7 @@ void print_error(info_t *info, char *estr)
 {
 	_eputs(info->fname);
 	_eputs(": ");
-	print_d(info->line_count, STDERR_FILEO);
+	print_d(info->line_count, STDERR_FILENO);
 	_eputs(": ");
 	_eputs(info->argv[0]);
 	_eputs(": ");
